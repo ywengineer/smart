8e82d69c1d40df0222d1c5e87c96b6d138ecf417
yangwei <ywengineer@gmail.com>
2022-05-01 16:36:52 +0800

    * refactor channel initializer for channel handler.

--- channel_initializers.go
+++ channel_initializers.go
@@ -24,8 +24,32 @@ func WithByteOrder(f func() binary.ByteOrder) ChannelInitializer {
 	}
 }
 
-func AddChannelHandler(f func() ChannelHandler) ChannelInitializer {
+func AddLastHandler(f func() ChannelHandler) ChannelInitializer {
 	return func(channel *SocketChannel) {
 		channel.handlers = append(channel.handlers, f())
+	}
+}
+
+func AddFirstHandler(f func() ChannelHandler) ChannelInitializer {
+	return func(channel *SocketChannel) {
+		channel.handlers = append([]ChannelHandler{f()}, channel.handlers...)
+	}
+}
+
+func AddHandlerAt(f func() ChannelHandler, pos int) ChannelInitializer {
+	if pos <= 0 {
+		return AddFirstHandler(f)
+	}
+	return func(channel *SocketChannel) {
+		if pos <= 0 {
+			AddFirstHandler(f)(channel)
+		} else if pos >= len(channel.handlers) {
+			AddLastHandler(f)(channel) // channel.handlers = append(channel.handlers, f())
+		} else {
+			hs := channel.handlers[:pos]
+			hs = append(hs, f())
+			hs = append(hs, channel.handlers[pos:]...)
+			channel.handlers = hs
+		}
 	}
 }
